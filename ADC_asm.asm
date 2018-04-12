cb8fea46c17ca9dbb510a9612554be0fc11e8b4c
UvOS <uvittos@gmail.com>
2018-04-12 02:18:40 -0500

    Update and rename ESM_ADC_asm.asm to ADC_asm.asm

--- ADC_asm.asm
+++ ADC_asm.asm
@@ -6,11 +6,11 @@
     org 00h
     goto inicio
 inicio movlw 0Eh    	
-    movwf ADCON1    ;para DAC
+    movwf ADCON1      ;para DAC
     movlw 01h	      ;00000001
-    movwf ADCON0    ;para DAC
+    movwf ADCON0      ;para DAC
     movlw 11h	      ;00010001
-    movwf ADCON2    ;para DAC
+    movwf ADCON2      ;para DAC
     movlw 01h
     clrf ADRESH
     clrf ADRESL    
@@ -18,8 +18,8 @@ inicio movlw 0Eh
     movwf TRISA
     clrf TRISD
     clrf PORTD
-main bsf ADCON0,1   ;<1>GO/DONE conv en progreso
-conv btfsc ADCON0,1 ;salta la terminar la conversion
+main bsf ADCON0,1     ;<1>GO/DONE conv en progreso
+conv btfsc ADCON0,1   ;salta la terminar la conversion
     goto conv
     movff ADRESH,PORTD
     goto main   
